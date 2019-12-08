29bc7a11cb23184927c3b5861fa607f64e35eb30
Matti Paksula <matti.paksula@iki.fi>
2019-12-08 17:46:19 +0200

    :sunrise:

---
+++ lib/worker.rb
@@ -0,0 +1,6 @@
+require "worker/version"
+
+module Worker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/worker_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Worker do
+  it "has a version number" do
+    expect(Worker::VERSION).not_to be nil
+  end
+end
